e2fd0ff6e155e5723041f8148fc8b90fbc6caf9b
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-09-19 10:51:24 +0000

    Done frontend for registration page

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+// function togglePasswordVisibility() {
+//     const passwordFields = [
+//         document.querySelector('input[name="password1"]'),
+//         document.querySelector('input[name="password2"]')
+//     ];
+//     passwordFields.forEach(field => {
+//         field.type = field.type === 'password' ? 'text' : 'password';
+//     });
+// }
