81f2f04fc328cf46675dafd4c775697fbcc1dcb1
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-09-24 20:59:46 +0000

    Allowed hosts

--- neighborhood_watch/settings.py
+++ neighborhood_watch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+-@@904)s6%hkyzeb_p%&dhj985n-p9jy1y=c$ype4(hb7j8t)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
